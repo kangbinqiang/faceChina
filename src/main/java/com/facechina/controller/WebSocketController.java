b4a1d8350fcae2e9f2386715363bf1ee6f113aaa
Kangbinqiang <18822253832@163.com>
2019-12-03 15:58:04 +0800

    websocket

---
+++ src/main/java/com/facechina/controller/WebSocketController.java
@@ -0,0 +1,31 @@
+package com.facechina.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@Api(tags = "WebSocket控制器")
+@RequestMapping("/websocket")
+public class WebSocketController {
+
+    @GetMapping("/{name}")
+    @ApiOperation(value = "客户端连接")
+    public String webSocket(@PathVariable(value = "name") String name, Model model) {
+        try {
+            log.info("跳转到websocket的页面上");
+            model.addAttribute("username", name);
+            return "websocket";
+        } catch (Exception e) {
+            log.info("跳转到websocket的页面上发生异常，异常信息是：" + e.getMessage());
+            return "error";
+        }
+    }
+}
